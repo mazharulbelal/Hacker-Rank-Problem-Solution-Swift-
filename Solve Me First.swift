a297e40786f2ef192363974d59379c68f654aa89
Md Mazharul Islam <ios.belal@gmail.com>
2017-04-02 11:41:50 +0600

    Add files via upload

---
+++ Solve Me First.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Mazharul on 4/2/17.
+//  Copyright © 2017 Mazharul. All rights reserved.
+//
+
+import Foundation
+
+// read integers line by line
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+
+// Hint: Type print(a + b) below
+var c = a+b
+print(c)
