d2c16f4f1ab3892bc536c160f15123fdf3c61342
Md Mazharul Islam <ios.belal@gmail.com>
2017-04-02 11:34:04 +0600

    Add files via upload

---
+++ 30 Days of Code Challenges/Day 0 Hello World.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Mazharul on 4/2/17.
+//  Copyright © 2017 Mazharul. All rights reserved.
+//
+
+import Foundation
+
+let input = readLine()!
+
+print("Hello, World. ")
+print(input)
